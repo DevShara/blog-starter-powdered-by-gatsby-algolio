cd1a2617173d86e4eab8f22abacd9e8860056fc6
DevShara <developer.ishara@gmail.com>
2022-12-26 08:27:31 -0800

    feat: complete navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,20 +1,26 @@
 import React from "react";
 
 const Navbar = () => (
-  <nav className="	p-2 bg-gray-800 " >
+  <nav className="p-1 bg-white-800 shadow-md " >
     <div className="container">
       <div className="flex flex-col items-center">
-        <h1 className="text-white m-3 font-bold text-2xl ">THE BLOG</h1>
-        <div className="">
+        <h1 className="text-inherit m-3 font-bold text-3xl "><a href="#">THE BLOG</a></h1>
+        <div className="border-t border-gray- p-2">
           <ul class="flex">
             <li class="mr-6">
-              <a class="text-blue-500 hover:text-blue-800" href="#">Active</a>
+              <a class="text-gray-400  hover:text-black" href="#">Spotlight</a>
             </li>
             <li class="mr-6">
-              <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+              <a class="text-gray-400  hover:text-black" href="#">Insights</a>
             </li>
             <li class="mr-6">
-              <a class="text-blue-500 hover:text-blue-800" href="#">Link</a>
+              <a class="text-gray-400  hover:text-black" href="#">History & Culture</a>
+            </li>
+            <li class="mr-6">
+              <a class="text-gray-400  hover:text-black" href="#">Identities</a>
+            </li>
+            <li class="mr-6">
+              <a class="text-gray-400  hover:text-black" href="#">Travel</a>
             </li>
           </ul>
         </div>
